a9c4628f3bd0ec0e4b8ef303854b9db3e6c5c265
Will Beaumont <wbeaumo1@gmail.com>
2024-12-08 14:30:57 -0800

    Updates sassOptions.style to "expanded"

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -3,75 +3,56 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const miniSVGDataURI = require("mini-svg-data-uri");
 
 module.exports = {
-    name: "app",
-    entry: "./src/js/app.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "app.[contenthash].js",
+    name      : "app", entry: "./src/js/app.js", output: {
+        path               : path.resolve(__dirname, "dist"),
+        filename           : "app.[contenthash].js",
         assetModuleFilename: "assets/[hash][ext]",
-    },
-    devtool: "source-map",
-    module: {
+    }, devtool: "source-map", module: {
         rules: [
             {
-                test: /\.s?css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: "css-loader",
-                        options: {
-                            importLoaders: 2,
-                            sourceMap: true,
+                test: /\.s?css$/, use: [
+                    MiniCssExtractPlugin.loader, {
+                        loader: "css-loader", options: {
+                            importLoaders: 2, sourceMap: true,
                         },
-                    },
-                    {
-                        loader: "postcss-loader",
-                        options: {
+                    }, {
+                        loader: "postcss-loader", options: {
                             postcssOptions: {
                                 plugins: [[require("autoprefixer")]],
-                            },
-                            sourceMap: true,
+                            }, sourceMap  : true,
                         },
-                    },
-                    {
-                        loader: "sass-loader",
-                        options: {
-                            sassOptions: {
-                                quietDeps: true,
-                                silenceDeprecations: [
+                    }, {
+                        loader: "sass-loader", options: {
+                            sassOptions : {
+                                quietDeps              : true,
+                                silenceDeprecations    : [
                                     "import",
                                     "color-functions",
                                     "global-builtin",
                                     "mixed-decls",
                                 ],
-                                sourceMap: true,
+                                sourceMap              : true,
                                 sourceMapIncludeSources: true,
-                            },
-                            sourceMap: true,
+                                style                  : "expanded",
+                            }, sourceMap: true,
                         },
                     },
                 ],
-            },
-            {
-                test: /\.svg$/,
-                type: "asset/inline",
-                generator: {
+            }, {
+                test  : /\.svg$/, type: "asset/inline", generator: {
                     dataUrl(content) {
                         content = content.toString();
                         return miniSVGDataURI(content);
                     },
-                },
-                use: "svgo-loader",
-            },
-            {
+                }, use: "svgo-loader",
+            }, {
                 test: /\.(eot|gif|jpe?g|otf|png|ttf|web[mp]|woff2?)$/,
                 type: "asset/resource",
             },
         ],
-    },
-    plugins: [
+    }, plugins: [
         new MiniCssExtractPlugin({
-            filename: "app.[contenthash].css",
-        }),
+                                     filename: "app.[contenthash].css",
+                                 }),
     ],
 };
